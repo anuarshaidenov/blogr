1a8af1a2a7e251ce1db490b582b3fb9c7ad4796a
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-09-26 22:43:14 +0600

    added hover state for links on both mobile and desktop css

--- index.js
+++ index.js
@@ -3,6 +3,35 @@ const hamburger = document.getElementById("hamburger");
 const closeIcon = document.getElementById("close-icon");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const mobileLink = document.querySelectorAll(
+  ".mobile-menu--links__main--link__a"
+);
+
+// const desktopLink = document.querySelector(".navbar--links__main");
+
+// desktopLink.addEventListener("mouseover", function (e) {
+//   console.log(e.target);
+// });
+
+// desktopLink.forEach((el) =>
+//   el.addEventListener("mouseover", function (e) {
+//     console.log(e.target);
+//   })
+// );
+
+// desktopLink.forEach((el) =>
+//   el.addEventListener("mouseout", function (e) {
+//     console.log(e.target);
+//   })
+// );
+
+mobileLink.forEach((el) =>
+  el.addEventListener("click", function (e) {
+    e.target.classList.toggle("opened-link");
+    e.target.parentNode.childNodes[2].classList.toggle("hidden");
+  })
+);
+
 hamburger.addEventListener("click", function () {
   [backdrop, mobileMenu, closeIcon].forEach((el) =>
     el.classList.remove("hidden")
